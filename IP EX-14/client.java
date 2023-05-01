215f532cd7ac5403eb3bb9a43d1f6ce372696cf0
Menaga Raman <116909288+menaga16@users.noreply.github.com>
2023-05-01 23:05:49 +0530

    Update client.java

--- IP EX-14/client.java
+++ IP EX-14/client.java
@@ -1,52 +1,14 @@
-package lab;
+package client;
 
-import java.sql.*;
-import javax.jws.WebService;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-
-/**
- *
- * @author 21cse075
- */
-@WebService(serviceName = "Ex14")
-public class Ex14 {
-
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) 
-   {
-        return "Hello " + txt + " !";
+public class clientside {
+    public static void main(String[] args) {
+        System.out.println(getDetails(001));
     }
 
-   
-    @WebMethod(operationName = "search")
-    public String search(@WebParam(name = "pid") String id) {
-        //TODO write your implementation code here:
-        boolean flag=false;
-        try{
-            Connection c=DriverManager.getConnection("jdbc:derby://localhost:1527/Products");
-            Statement st=c.createStatement();
-            ResultSet rs=st.executeQuery("select * from product");
-            while(rs.next()){
-                String id1=rs.getString("Pid");
-                if(id1.equals(id)){
-                    flag=true;
-                    break;
-                }
-            }
-           
-            if(flag){
-             
-                return "true";
-            }
-            else
-                return "false";
-        }
-        catch(Exception e)
-        {
-            
-        }
-       
+    private static String getDetails(int id) {
+        lab.Ex14_Service service = new lab.Ex14_Service();
+        lab.Ex14 port = service.getEx14Port();
+        return port.getDetails(id);
     }
-       
-    }
+    
+}
